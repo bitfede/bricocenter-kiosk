9c3609dcd4a18fd5ef69759fb0743cf71ef5ecda
bitfede <f.g.defaveri@gmail.com>
2018-05-01 17:50:52 +0200

    Adds vanilla javascript to swap videos dynamically and styles further

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// MAIN VARS
+var mainvideo = document.getElementById('mainvideo')
+var mainvideosrc = document.getElementById('mainvidsrc')
+
+var videothumbs = document.getElementsByClassName('thumbvideo')
+
+for(var z = 0; z < videothumbs.length; z++) {
+    var elem = videothumbs[z];
+    console.log("ELEM ", elem)
+    elem.onclick = function(e) {
+        var newsrc = e.target.childNodes[1].src
+        console.log("clicked ", newsrc)
+        // alert("hello"); //DEBUG
+        mainvideosrc.setAttribute('src', newsrc)
+        mainvideo.appendChild(mainvideosrc);
+        mainvideo.load()
+};
+}
